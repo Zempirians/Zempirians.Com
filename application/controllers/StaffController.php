540494653b8bb351a8311c944afe831a1bd89ec8
UberLame <w0rm@sploit.me.uk>
2014-01-08 14:29:07 -0600

    Built the staff control panel
    All set to go

--- application/controllers/StaffController.php
+++ application/controllers/StaffController.php
@@ -35,7 +35,7 @@ class StaffController extends Zend_Controller_Action
 	{
 		$wmf_ns           = new Zend_Session_Namespace('SPLOIT');
 		$wmf_ns->page     = "staff";
-		$wmf_ns->mod      = "structure";
+		$wmf_ns->mod      = "curriculum";
 		$wmf_ns->descrip  = "click";
 		$wmf_ns->grant    = "yes";
 		$wmf_ns->rights   = $this->_helper->myprofile->numtoval('worm');
@@ -51,11 +51,11 @@ class StaffController extends Zend_Controller_Action
 		$picklayout = $this->_helper->layout->setLayout('staff.layout');
 	}
 
-	function courseAction()
+	function lookcurriculumAction()
 	{
 		$wmf_ns           = new Zend_Session_Namespace('SPLOIT');
 		$wmf_ns->page     = "staff";
-		$wmf_ns->mod      = "course";
+		$wmf_ns->mod      = "lookcurriculum";
 		$wmf_ns->descrip  = "click";
 		$wmf_ns->grant    = "yes";
 		$wmf_ns->rights   = $this->_helper->myprofile->numtoval('worm');
@@ -65,17 +65,20 @@ class StaffController extends Zend_Controller_Action
 		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
 		$this->view->zempirian = $userInfo;
 
-		$newlist = $this->_helper->staff->schoollist('2');
-		$this->view->newlist = $newlist;
+		$a = $this->getRequest()->getParam('stdin');
+		$boola = $this->_helper->shieldsup->paramnum($a);
+
+		if ($boola == "YAY") { $this->view->stdin = $a; }
+		else { $this->_redirect('shield/trap'); }
 
 		$picklayout = $this->_helper->layout->setLayout('staff.layout');
 	}
 
-	function lessonAction()
+	function editcurriculumAction()
 	{
 		$wmf_ns           = new Zend_Session_Namespace('SPLOIT');
 		$wmf_ns->page     = "staff";
-		$wmf_ns->mod      = "lesson";
+		$wmf_ns->mod      = "editcurriculum";
 		$wmf_ns->descrip  = "click";
 		$wmf_ns->grant    = "yes";
 		$wmf_ns->rights   = $this->_helper->myprofile->numtoval('worm');
@@ -85,8 +88,11 @@ class StaffController extends Zend_Controller_Action
 		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
 		$this->view->zempirian = $userInfo;
 
-		$newlist = $this->_helper->staff->schoollist('3');
-		$this->view->newlist = $newlist;
+		$a = $this->getRequest()->getParam('stdin');
+		$boola = $this->_helper->shieldsup->paramnum($a);
+
+		if ($boola == "YAY") { $this->view->stdin = $a; }
+		else { $this->_redirect('shield/trap'); }
 
 		$picklayout = $this->_helper->layout->setLayout('staff.layout');
 	}
@@ -139,6 +145,106 @@ class StaffController extends Zend_Controller_Action
 
 	}
 
+	function curriculumsaveAction()
+	{
+		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
+
+		$wmf_ns          	= new Zend_Session_Namespace('SPLOIT');
+		$wmf_ns->page    	= "staff";
+		$wmf_ns->mod     	= "curriculumsave";
+		$wmf_ns->descrip 	= "submit";
+		$wmf_ns->grant   	= "yes";
+		$wmf_ns->stats		= $this->_helper->pagestats->log($wmf_ns->page,$wmf_ns->mod);
+		$this->_helper->shieldsup->surflog($wmf_ns->mod,$wmf_ns->page,$wmf_ns->descrip,'1');
+		$this->_helper->layout->setLayout('staff.layout');
+		$this->view->zempirian = $userInfo;
+
+		$a = $this->getRequest()->getParam('s_name');
+		$b = $this->getRequest()->getParam('s_level');
+		$c = $this->getRequest()->getParam('s_other');
+		$d = $this->getRequest()->getParam('stdin');
+		$boola = $this->_helper->shieldsup->paramtxt($a);
+		$boolb = $this->_helper->shieldsup->paramnum($b);
+		$boolc = $this->_helper->shieldsup->paramtxt($c);
+		$boold = $this->_helper->shieldsup->paramnum($d);
+
+		if ($boola == "YAY" && $boolb == "YAY" && $boolc == "YAY" && $boold == "YAY") {
+			$a = $this->_helper->shieldsup->paramfilter($a);
+			$c = $this->_helper->shieldsup->paramfilter($c);
+			$decide = $this->_helper->staff->savecurriculum($a,$b,$c,$d);
+			if ($decide == "YAY") { $this->_redirect('staff/curriculum'); }
+			else { $this->_redirect('shield/trap'); }
+		}
+		else { $this->_redirect('shield/trap'); }
+
+	}
+
+	function courseAction()
+	{
+		$wmf_ns           = new Zend_Session_Namespace('SPLOIT');
+		$wmf_ns->page     = "staff";
+		$wmf_ns->mod      = "course";
+		$wmf_ns->descrip  = "click";
+		$wmf_ns->grant    = "yes";
+		$wmf_ns->rights   = $this->_helper->myprofile->numtoval('worm');
+		$wmf_ns->stats    = $this->_helper->pagestats->log($wmf_ns->page,$wmf_ns->mod);
+
+		$this->_helper->shieldsup->surflog($wmf_ns->mod,$wmf_ns->page,$wmf_ns->descrip,'1');
+		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
+		$this->view->zempirian = $userInfo;
+
+		$newlist = $this->_helper->staff->schoollist('2');
+		$this->view->newlist = $newlist;
+
+		$picklayout = $this->_helper->layout->setLayout('staff.layout');
+	}
+
+	function lookcourseAction()
+	{
+		$wmf_ns           = new Zend_Session_Namespace('SPLOIT');
+		$wmf_ns->page     = "staff";
+		$wmf_ns->mod      = "lookcourse";
+		$wmf_ns->descrip  = "click";
+		$wmf_ns->grant    = "yes";
+		$wmf_ns->rights   = $this->_helper->myprofile->numtoval('worm');
+		$wmf_ns->stats    = $this->_helper->pagestats->log($wmf_ns->page,$wmf_ns->mod);
+
+		$this->_helper->shieldsup->surflog($wmf_ns->mod,$wmf_ns->page,$wmf_ns->descrip,'1');
+		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
+		$this->view->zempirian = $userInfo;
+
+		$a = $this->getRequest()->getParam('stdin');
+		$boola = $this->_helper->shieldsup->paramnum($a);
+
+		if ($boola == "YAY") { $this->view->stdin = $a; }
+		else { $this->_redirect('shield/trap'); }
+
+		$picklayout = $this->_helper->layout->setLayout('staff.layout');
+	}
+
+	function editcourseAction()
+	{
+		$wmf_ns           = new Zend_Session_Namespace('SPLOIT');
+		$wmf_ns->page     = "staff";
+		$wmf_ns->mod      = "editcourse";
+		$wmf_ns->descrip  = "click";
+		$wmf_ns->grant    = "yes";
+		$wmf_ns->rights   = $this->_helper->myprofile->numtoval('worm');
+		$wmf_ns->stats    = $this->_helper->pagestats->log($wmf_ns->page,$wmf_ns->mod);
+
+		$this->_helper->shieldsup->surflog($wmf_ns->mod,$wmf_ns->page,$wmf_ns->descrip,'1');
+		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
+		$this->view->zempirian = $userInfo;
+
+		$a = $this->getRequest()->getParam('stdin');
+		$boola = $this->_helper->shieldsup->paramnum($a);
+
+		if ($boola == "YAY") { $this->view->stdin = $a; }
+		else { $this->_redirect('shield/trap'); }
+
+		$picklayout = $this->_helper->layout->setLayout('staff.layout');
+	}
+
 	function addcourseAction()
 	{
 		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
@@ -187,6 +293,164 @@ class StaffController extends Zend_Controller_Action
 		}
 		else { $this->_redirect('shield/trap'); }
 
+	}
+
+	function coursesaveAction()
+	{
+		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
+
+		$wmf_ns          	= new Zend_Session_Namespace('SPLOIT');
+		$wmf_ns->page    	= "staff";
+		$wmf_ns->mod     	= "coursesave";
+		$wmf_ns->descrip 	= "submit";
+		$wmf_ns->grant   	= "yes";
+		$wmf_ns->stats		= $this->_helper->pagestats->log($wmf_ns->page,$wmf_ns->mod);
+		$this->_helper->shieldsup->surflog($wmf_ns->mod,$wmf_ns->page,$wmf_ns->descrip,'1');
+		$this->_helper->layout->setLayout('staff.layout');
+		$this->view->zempirian = $userInfo;
+
+		$a = $this->getRequest()->getParam('s_name');
+		$b = $this->getRequest()->getParam('s_level');
+		$c = $this->getRequest()->getParam('s_other');
+		$d = $this->getRequest()->getParam('s_curriculum');
+		$e = $this->getRequest()->getParam('stdin');
+		$boola = $this->_helper->shieldsup->paramtxt($a);
+		$boolb = $this->_helper->shieldsup->paramnum($b);
+		$boolc = $this->_helper->shieldsup->paramtxt($c);
+		$boold = $this->_helper->shieldsup->paramnum($d);
+		$boole = $this->_helper->shieldsup->paramnum($e);
+
+		if ($boola == "YAY" && $boolb == "YAY" && $boolc == "YAY" && $boold == "YAY" && $boole == "YAY") {
+			$a = $this->_helper->shieldsup->paramfilter($a);
+			$c = $this->_helper->shieldsup->paramfilter($c);
+			$decide = $this->_helper->staff->savecourse($a,$b,$c,$d,$e);
+			if ($decide == "YAY") { $this->_redirect('staff/course'); }
+			else { $this->_redirect('shield/trap'); }
+		}
+		else { $this->_redirect('shield/trap'); }
+
+	}
+
+	function acceptcourseAction()
+	{
+		$wmf_ns           = new Zend_Session_Namespace('SPLOIT');
+		$wmf_ns->page     = "staff";
+		$wmf_ns->mod      = "acceptcourse";
+		$wmf_ns->descrip  = "submit";
+		$wmf_ns->grant    = "yes";
+		$wmf_ns->rights   = $this->_helper->myprofile->numtoval('worm');
+		$wmf_ns->stats    = $this->_helper->pagestats->log($wmf_ns->page,$wmf_ns->mod);
+
+		$this->_helper->shieldsup->surflog($wmf_ns->mod,$wmf_ns->page,$wmf_ns->descrip,'1');
+		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
+		$this->view->zempirian = $userInfo;
+
+		$a = $this->getRequest()->getParam('stdin');
+		$boola = $this->_helper->shieldsup->paramnum($a);
+		if ($boola == "YAY") {
+			$decide = $this->_helper->staff->course($a,"1");
+			if ($decide == "YAY") {
+				 $this->_redirect('staff/course');
+			}
+			else { $this->_redirect('shield/trap'); }
+		}
+		else { $this->_redirect('shield/trap');	}
+
+		$picklayout = $this->_helper->layout->setLayout('staff.layout');
+	}
+
+	function denycourseAction()
+	{
+		$wmf_ns           = new Zend_Session_Namespace('SPLOIT');
+		$wmf_ns->page     = "staff";
+		$wmf_ns->mod      = "denycourse";
+		$wmf_ns->descrip  = "submit";
+		$wmf_ns->grant    = "yes";
+		$wmf_ns->rights   = $this->_helper->myprofile->numtoval('worm');
+		$wmf_ns->stats    = $this->_helper->pagestats->log($wmf_ns->page,$wmf_ns->mod);
+
+		$this->_helper->shieldsup->surflog($wmf_ns->mod,$wmf_ns->page,$wmf_ns->descrip,'1');
+		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
+		$this->view->zempirian = $userInfo;
+
+		$a = $this->getRequest()->getParam('stdin');
+		$boola = $this->_helper->shieldsup->paramnum($a);
+		if ($boola == "YAY") {
+			$decide = $this->_helper->staff->course($a,"0");
+			if ($decide == "YAY") {
+				 $this->_redirect('staff/course');
+			}
+			else { $this->_redirect('shield/trap'); }
+		}
+		else { $this->_redirect('shield/trap');	}
+
+		$picklayout = $this->_helper->layout->setLayout('staff.layout');
+	}
+
+	function lessonAction()
+	{
+		$wmf_ns           = new Zend_Session_Namespace('SPLOIT');
+		$wmf_ns->page     = "staff";
+		$wmf_ns->mod      = "lesson";
+		$wmf_ns->descrip  = "click";
+		$wmf_ns->grant    = "yes";
+		$wmf_ns->rights   = $this->_helper->myprofile->numtoval('worm');
+		$wmf_ns->stats    = $this->_helper->pagestats->log($wmf_ns->page,$wmf_ns->mod);
+
+		$this->_helper->shieldsup->surflog($wmf_ns->mod,$wmf_ns->page,$wmf_ns->descrip,'1');
+		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
+		$this->view->zempirian = $userInfo;
+
+		$newlist = $this->_helper->staff->schoollist('3');
+		$this->view->newlist = $newlist;
+
+		$picklayout = $this->_helper->layout->setLayout('staff.layout');
+	}
+
+	function looklessonAction()
+	{
+		$wmf_ns           = new Zend_Session_Namespace('SPLOIT');
+		$wmf_ns->page     = "staff";
+		$wmf_ns->mod      = "looklesson";
+		$wmf_ns->descrip  = "click";
+		$wmf_ns->grant    = "yes";
+		$wmf_ns->rights   = $this->_helper->myprofile->numtoval('worm');
+		$wmf_ns->stats    = $this->_helper->pagestats->log($wmf_ns->page,$wmf_ns->mod);
+
+		$this->_helper->shieldsup->surflog($wmf_ns->mod,$wmf_ns->page,$wmf_ns->descrip,'1');
+		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
+		$this->view->zempirian = $userInfo;
+
+		$a = $this->getRequest()->getParam('stdin');
+		$boola = $this->_helper->shieldsup->paramnum($a);
+
+		if ($boola == "YAY") { $this->view->stdin = $a; }
+		else { $this->_redirect('shield/trap'); }
+
+		$picklayout = $this->_helper->layout->setLayout('staff.layout');
+	}
+
+	function editlessonAction()
+	{
+		$wmf_ns           = new Zend_Session_Namespace('SPLOIT');
+		$wmf_ns->page     = "staff";
+		$wmf_ns->mod      = "editlesson";
+		$wmf_ns->descrip  = "click";
+		$wmf_ns->grant    = "yes";
+		$wmf_ns->rights   = $this->_helper->myprofile->numtoval('worm');
+		$wmf_ns->stats    = $this->_helper->pagestats->log($wmf_ns->page,$wmf_ns->mod);
+
+		$this->_helper->shieldsup->surflog($wmf_ns->mod,$wmf_ns->page,$wmf_ns->descrip,'1');
+		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
+		$this->view->zempirian = $userInfo;
+
+		$a = $this->getRequest()->getParam('stdin');
+		$boola = $this->_helper->shieldsup->paramnum($a);
+
+		if ($boola == "YAY") { $this->view->stdin = $a; }
+		else { $this->_redirect('shield/trap'); }
+
+		$picklayout = $this->_helper->layout->setLayout('staff.layout');
 	}
 
 	function addlessonAction()
@@ -239,5 +503,99 @@ class StaffController extends Zend_Controller_Action
 		}
 		else { $this->_redirect('shield/trap'); }
 
+	}
+
+	function lessonsaveAction()
+	{
+		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
+
+		$wmf_ns          	= new Zend_Session_Namespace('SPLOIT');
+		$wmf_ns->page    	= "staff";
+		$wmf_ns->mod     	= "lessonsave";
+		$wmf_ns->descrip 	= "submit";
+		$wmf_ns->grant   	= "yes";
+		$wmf_ns->stats		= $this->_helper->pagestats->log($wmf_ns->page,$wmf_ns->mod);
+		$this->_helper->shieldsup->surflog($wmf_ns->mod,$wmf_ns->page,$wmf_ns->descrip,'1');
+		$this->_helper->layout->setLayout('staff.layout');
+		$this->view->zempirian = $userInfo;
+
+		$a = $this->getRequest()->getParam('s_name');
+		$b = $this->getRequest()->getParam('s_level');
+		$c = $this->getRequest()->getParam('s_other');
+		$d = $this->getRequest()->getParam('s_curriculum');
+		$e = $this->getRequest()->getParam('s_course');
+		$f = $this->getRequest()->getParam('stdin');
+		$boola = $this->_helper->shieldsup->paramtxt($a);
+		$boolb = $this->_helper->shieldsup->paramnum($b);
+		$boolc = $this->_helper->shieldsup->paramtxt($c);
+		$boold = $this->_helper->shieldsup->paramnum($d);
+		$boole = $this->_helper->shieldsup->paramnum($e);
+		$boolf = $this->_helper->shieldsup->paramnum($f);
+
+		if ($boola == "YAY" && $boolb == "YAY" && $boolc == "YAY" && $boold == "YAY" && $boole == "YAY" && $boolf == "YAY") {
+			$a = $this->_helper->shieldsup->paramfilter($a);
+			$c = $this->_helper->shieldsup->paramfilter($c);
+			$decide = $this->_helper->staff->savelesson($a,$b,$c,$d,$e,$f);
+			if ($decide == "YAY") { $this->_redirect('staff/lesson'); }
+			else { $this->_redirect('shield/trap'); }
+		}
+		else { $this->_redirect('shield/trap'); }
+
+	}
+
+	function acceptlessonAction()
+	{
+		$wmf_ns           = new Zend_Session_Namespace('SPLOIT');
+		$wmf_ns->page     = "staff";
+		$wmf_ns->mod      = "acceptlesson";
+		$wmf_ns->descrip  = "submit";
+		$wmf_ns->grant    = "yes";
+		$wmf_ns->rights   = $this->_helper->myprofile->numtoval('worm');
+		$wmf_ns->stats    = $this->_helper->pagestats->log($wmf_ns->page,$wmf_ns->mod);
+
+		$this->_helper->shieldsup->surflog($wmf_ns->mod,$wmf_ns->page,$wmf_ns->descrip,'1');
+		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
+		$this->view->zempirian = $userInfo;
+
+		$a = $this->getRequest()->getParam('stdin');
+		$boola = $this->_helper->shieldsup->paramnum($a);
+		if ($boola == "YAY") {
+			$decide = $this->_helper->staff->lesson($a,"1");
+			if ($decide == "YAY") {
+				 $this->_redirect('staff/lesson');
+			}
+			else { $this->_redirect('shield/trap'); }
+		}
+		else { $this->_redirect('shield/trap');	}
+
+		$picklayout = $this->_helper->layout->setLayout('staff.layout');
+	}
+
+	function denylessonAction()
+	{
+		$wmf_ns           = new Zend_Session_Namespace('SPLOIT');
+		$wmf_ns->page     = "staff";
+		$wmf_ns->mod      = "denylesson";
+		$wmf_ns->descrip  = "submit";
+		$wmf_ns->grant    = "yes";
+		$wmf_ns->rights   = $this->_helper->myprofile->numtoval('worm');
+		$wmf_ns->stats    = $this->_helper->pagestats->log($wmf_ns->page,$wmf_ns->mod);
+
+		$this->_helper->shieldsup->surflog($wmf_ns->mod,$wmf_ns->page,$wmf_ns->descrip,'1');
+		$userInfo = Zend_Auth::getInstance()->getStorage()->read();
+		$this->view->zempirian = $userInfo;
+
+		$a = $this->getRequest()->getParam('stdin');
+		$boola = $this->_helper->shieldsup->paramnum($a);
+		if ($boola == "YAY") {
+			$decide = $this->_helper->staff->lesson($a,"0");
+			if ($decide == "YAY") {
+				 $this->_redirect('staff/lesson');
+			}
+			else { $this->_redirect('shield/trap'); }
+		}
+		else { $this->_redirect('shield/trap');	}
+
+		$picklayout = $this->_helper->layout->setLayout('staff.layout');
 	}
 }
